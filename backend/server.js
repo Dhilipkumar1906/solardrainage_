9926558fddc440be4b067760ff14ab20003eec21
Dhilipkumar M <dhilipmari916@gmail.com>
2025-12-17 16:48:58 +0530

    backend ready

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cron = require("node-cron");
+const cors = require("cors");
+require("dotenv").config();
+
 const fetchThingSpeak = require("./cron/fetchThingSpeak");
 const lidRoutes = require("./routes/lidRoutes");
-const cors = require("cors");
-
-
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/solardrainage")
-  .then(() => console.log("✅ MongoDB Connected"));
+// ✅ LOCAL MONGODB ONLY
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Atlas Connected"))
+  .catch(err => console.error(err));
+
 
 app.use("/api/lids", lidRoutes);
 
-// every 15 seconds
-// cron.schedule("*/15 * * * * *", fetchThingSpeak);
+// ✅ LOCAL CRON ENABLED
+cron.schedule("*/15 * * * * *", fetchThingSpeak);
 
-app.listen(5000, () =>
-  console.log("🚀 Server running on port 5000")
-);
+app.listen(5000, () => {
+  console.log("🚀 Local server running on http://localhost:5000");
+});
